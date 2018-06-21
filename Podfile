100052f24a41b950378353e349955c7f27835d97
tb212304_2013 <848967151@qq.com>
2018-06-22 07:06:52 +0800

    GPUImage

---
+++ Podfile
@@ -0,0 +1,10 @@
+
+
+
+target 'PictureControl' do
+
+pod 'GPUImage', '~> 0.1.7'
+
+end
+
+
---
+++ Pods/Target Support Files/Pods-PictureControl/Pods-PictureControl-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PictureControl : NSObject
+@end
+@implementation PodsDummy_Pods_PictureControl
+@end
